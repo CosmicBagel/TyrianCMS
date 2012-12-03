f2cbf87726391761632804f69ee8cfba2392e81b
Samuel Whiteley <bling.nuke@gmail.com>
2012-12-02 17:43:10 -0700

    Inital commit of the rails project (just the rails project that is created by default)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TyrianCMS::Application.config.secret_token = 'e4cb6c923c055ca50b9e5f6eabda63671423d849f7e202f8e1eeaf5cc205f8f2ed48ef83076ac19fc82be425b258ff309229f5490666a6a65e77516227e5c10d'
